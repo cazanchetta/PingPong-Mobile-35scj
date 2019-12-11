78ecd1e29ec0cabafbde45514cc73d2dc1618172
Logon Aluno <logonaluno@local.com>
2019-12-10 21:53:12 -0300

    Mobile

--- app/src/main/java/br/com/heider/pingpong/MainActivity.kt
+++ app/src/main/java/br/com/heider/pingpong/MainActivity.kt
@@ -2,6 +2,7 @@ package br.com.heider.pingpong
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,6 +10,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        
+        setUpView()
+    }
+
+    private fun setUpView() {
+        tvHomeName.text = intent.getStringExtra("HOME_NAME")
+        tvAwayName.text = intent.getStringExtra("AWAY_NAME")
+        setUpListener()
+    }
+
+    private fun setUpListener() {
+        btHomeScore.setOnClickListener{
+            
+        }
+
+        btAwayScore.setOnClickListener{
+
+        }
     }
 }
